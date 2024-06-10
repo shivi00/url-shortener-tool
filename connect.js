4938b1dc4941be0e14aa68063dacce6510201266
shivi00 <you@example.com>
2024-06-10 12:45:51 +0530

    Changes in connect and controller

--- connect.js
+++ connect.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+
+// Queries out all the documents with matching schema otherwise gives error or empty result. ?It can be removed so that not matched queries can also be put in result.
 mongoose.set("strictQuery", true);
 async function connectToMongoDB(url) {
   return mongoose.connect(url);
--- controllers/url.js
+++ controllers/url.js
@@ -28,18 +28,29 @@ async function handleperclick(req,res){
   const shortId = req.params.shortId;
   const entry = await URL.findOneAndUpdate(
     {
+
+      // This query is taken to match the shortId field of the document
       shortId,
     },
+
+    // This is update object defines how to update the document
     {
 
-      
+      // for pushing the data inside the array
       $push: {
+
+        // Defines to push the new object to the visitHistory array of the found document
         visitHistory: {
+
+          // Object has the single property. Only the object property which is defined in schema will be pushed to the array.
           timestamp: Date.now(),
+
         },
       },
     }
   );
+
+  // send the redirect the response to the browser instructing it to navigate the url.
   res.redirect(entry.redirectURL);
 }
 
@@ -49,3 +60,4 @@ module.exports = {
   handleperclick,
 };
 
+// nano id is used to generate an id of the character.
